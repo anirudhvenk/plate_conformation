8cf511f9ff37a4b901b5d15faded70841bff224a
Anirudh Venkatraman <anirudhv05ca@gmail.com>
2023-10-18 14:32:31 -0500

    initial commit

---
+++ data.py
@@ -0,0 +1,32 @@
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from PIL import Image
+import torchvision.transforms as T
+import os
+
+
+class ImageFolder(Dataset):
+
+    def __init__(self, root):
+        imgs = [os.path.join(root, img_path) for img_path in os.listdir(root)]
+
+        self.root = root
+        self.imgs = imgs
+
+    def __len__(self):
+        return len(self.imgs)
+
+    def __getitem__(self, index):
+        path = self.imgs[index]
+        img = Image.open(path)
+        # img = T.functional.crop(img, 600,850,450,450)
+        img = T.functional.crop(img, 100,1200,1900,1400)
+        img = T.ToTensor()(img)
+        
+        if os.path.basename(path)[0] == '1':
+            return img, 1
+        else:
+            return img, 0
+
---
+++ train.py
@@ -0,0 +1,44 @@
+import torch
+from torchvision import models
+from torch import nn
+from data import ImageFolder
+from torch.nn.modules.loss import BCEWithLogitsLoss
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import os
+
+training_data = ImageFolder('./pictures/train')
+testing_data = ImageFolder('./pictures/test')
+train_dataloader = DataLoader(training_data, batch_size=1, shuffle=True)
+test_dataloader = DataLoader(testing_data, batch_size=1, shuffle=True)
+
+model = models.resnet18(pretrained=False)
+nr_filters = model.fc.in_features
+model.fc = nn.Linear(nr_filters, 1)
+
+loss_fn = BCEWithLogitsLoss()
+optimizer = torch.optim.Adam(model.fc.parameters())
+
+for epoch in range(10):
+    epoch_loss = 0
+    for img, y in tqdm(train_dataloader):
+        optimizer.zero_grad()
+        y_hat = model(img)
+        loss = loss_fn(y_hat, y.unsqueeze(1).float())
+        loss.backward()
+        optimizer.step()
+        
+        epoch_loss += loss/len(train_dataloader)
+    
+    print(epoch_loss.item())
+    
+    with torch.no_grad():
+        cum_loss = 0
+        for img, y in tqdm(test_dataloader):
+            y_hat = model(img)
+            loss = loss_fn(y_hat, y.unsqueeze(1).float())
+            cum_loss += loss/len(test_dataloader)
+        
+        print(cum_loss.item())
+        
+    torch.save(model.state_dict(), f'weights/model.ckpt.{epoch}')
