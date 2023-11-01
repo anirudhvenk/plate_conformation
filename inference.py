54c28e001f6f90b59ddf357722c87b2a7f859912
anirudhvenk <anirudhv05ca@gmail.com>
2023-11-01 22:36:36 +0000

    changed up model/cropped images

--- inference.py
+++ inference.py
@@ -4,17 +4,22 @@ import torchvision.transforms as T
 from torchvision import datasets, models, transforms
 import os
 from PIL import Image
+from model import Net
 
-for img in os.listdir('./pictures/test'):
-    img = Image.open(os.path.join('./pictures/test', img))
-    img = T.functional.crop(img, 600,850,450,450)
-    img = T.ToTensor()(img)
+model = models.resnet50(pretrained=True)
+nr_filters = model.fc.in_features
+model.fc = nn.Sequential(
+    nn.Dropout(0.5),
+    nn.Linear(nr_filters, 1),
+)
+model.load_state_dict(torch.load('weights/model.ckpt.7'))
+model.eval()
+model.to('cuda')
 
-    model = models.resnet18(pretrained=False)
-    nr_filters = model.fc.in_features
-    model.fc = nn.Linear(nr_filters, 1)
 
-    model.load_state_dict(torch.load('weights/model.ckpt.9'))
-    model.eval()
-    print(torch.sigmoid(model(img.unsqueeze(0))))
-
+for img in os.listdir('./pictures/cropped_test/backward'):
+    img = Image.open(os.path.join('./pictures/cropped_test/backward', img))
+    img = T.Resize(size=(224,224))(img)
+    img = T.ToTensor()(img).unsqueeze(0)
+    print(torch.sigmoid(model(img.to('cuda'))))
+    
--- train.py
+++ train.py
@@ -1,46 +1,72 @@
 import torch
-from torchvision import models
+from torchvision import models, datasets, transforms
 from torch import nn
-from data import ImageFolder
 from torch.nn.modules.loss import BCEWithLogitsLoss
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 import os
+from model import Net
 
-training_data = ImageFolder('./pictures/train')
-testing_data = ImageFolder('./pictures/test2')
-train_dataloader = DataLoader(training_data, batch_size=10, shuffle=True)
-test_dataloader = DataLoader(testing_data, batch_size=1, shuffle=True)
+train_transforms = transforms.Compose([
+    transforms.Resize(size=(232,232)),
+    transforms.CenterCrop((224,224)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225],
+    ),
+])
+test_transforms = transforms.Compose([
+    transforms.Resize(size=(232,232)),
+    transforms.CenterCrop((224,224)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225],
+    ),
+])
 
-model = models.resnet18(pretrained=False)
+train_data = datasets.ImageFolder('./pictures/cropped_train',transform=train_transforms)
+test_data = datasets.ImageFolder('./pictures/cropped_test',transform=test_transforms)
+
+trainloader = DataLoader(train_data, shuffle=True, batch_size=3)
+testloader = DataLoader(test_data, shuffle=True, batch_size=3)
+
+model = models.resnet50(pretrained=True)
+for params in model.parameters():
+  params.requires_grad_ = False
+
 nr_filters = model.fc.in_features
 model.fc = nn.Sequential(
     nn.Dropout(0.5),
     nn.Linear(nr_filters, 1),
 )
+model.to('cuda')
 
 loss_fn = BCEWithLogitsLoss()
-optimizer = torch.optim.Adam(model.fc.parameters())
+optimizer = torch.optim.Adam(model.fc.parameters(), lr=0.0005)
 
-for epoch in range(10):
+for epoch in range(50):
     epoch_loss = 0
-    for img, y in tqdm(train_dataloader):
+    for img, y in tqdm(trainloader):
         optimizer.zero_grad()
-        y_hat = model(img)
-        loss = loss_fn(y_hat, y.unsqueeze(1).float())
+        y_hat = model(img.to('cuda'))
+        loss = loss_fn(y_hat, y.unsqueeze(1).float().cuda())
         loss.backward()
         optimizer.step()
         
-        epoch_loss += loss/len(train_dataloader)
+        epoch_loss += loss/len(trainloader)
     
     print(epoch_loss.item())
     
     with torch.no_grad():
         cum_loss = 0
-        for img, y in tqdm(test_dataloader):
-            y_hat = model(img)
-            loss = loss_fn(y_hat, y.unsqueeze(1).float())
-            cum_loss += loss/len(test_dataloader)
+        for img, y in tqdm(testloader):
+            y_hat = model(img.to('cuda'))
+            loss = loss_fn(y_hat, y.unsqueeze(1).float().cuda())
+            print(torch.sigmoid(y_hat))
+            print(y.unsqueeze(1))
+            cum_loss += loss/len(testloader)
         
         print(cum_loss.item())
         
